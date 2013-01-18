5c4286f4abddd3b5726a34f3f1a3d25f000003fa
Jakub Buczyński <jbuczynski@sigma.inf.ug.edu.pl>
2013-01-19 00:55:06 +0100

    wstepny zarys jeszcze trzeba dopisac kilka funkcji

--- skrypt.sh
+++ skrypt.sh
@@ -2,38 +2,46 @@
 
 #!/bin/bash
 baza='ble';
-licznik=0;
+licznik=1;
 zmienna="x";
 imie='ble';
 nazwisko='ble';
 wiek='0';
-function wybor
+
+
+
+if [ -d $(pwd)/baza ]
+then
+echo;
+else
+mkdir $(pwd)/baza;
+fi
+
+
+function menu_wybor
 {
 case $zmienna in
-d) dane;
+dodaj) dane;
  licznik=licznik+1;;
-e)wejscie;;
-w) echo "Dostepne Bazy, aby wyjs do menu- enter /n ";
+
+wyswietl) cd $(pwd)/baza;
+echo "Dostepne Pliki /n ";
 ls;
 read;;
+sortuj)echo;;
+wybierz)echo;;
+usun)echo;;
 esac;
 
 }
 
-function wejscie
-{
-ls;
-echo;
-echo "podaj nazwe bazy do której chcesz wejsc";
-read baza;
-cd baza;
-}
+
 
 function dane
 {
 
-mkdir $(pwd) baza$licznik;
-cd baza$licznik;
+
+cd baza;
 
 echo "Podaj imie";
 read imie;
@@ -45,11 +53,11 @@ echo "Podaj wiek";
 read wiek;
 echo;
 
-touch osoba$licznik.txt;
-echo "ID: &licznik" >> osoba$licznik.txt ;
-echo "Imie: &imie" >> osoba$licznik.txt  ;
-echo "Nazwisko: &nazwisko" >> osoba$licznik.txt ;
-echo "Wiek: &wiek" >> osoba$licznik.txt ;
+touch $licznik.txt;
+echo "ID: $licznik" >> osoba$licznik.txt ;
+echo "Imie: $imie" >> osoba$licznik.txt  ;
+echo "Nazwisko: $nazwisko" >> osoba$licznik.txt ;
+echo "Wiek: $wiek" >> osoba$licznik.txt ;
 
 cd ..
 }
@@ -64,12 +72,16 @@ clear; # czyść ekran
 
 echo " Menu ";
 echo;
-echo "d -  Dodaj dane osobowe.";
-echo "w - wyświetl wszystkie bazy w glownym katalogu.";
-echo "e aby wejsc do wybranej bazy";
+echo "wpisz  odpowiednia fraze a nastepnie nacisnij enter" ;
+echo;
+echo "dodaj - Dodaj dane osobowe.";
+echo "wyswietl - wyświetl wszystkie pliki w bazie .";
+echo "sortuj - sortuj wg. nazwiska";
+echo "wybierz -  wybierz plik do wyswietlenia";
+echo "usun - wybierz plik do usuniecia";
 echo "k - zakończ skrypt.";
 read zmienna;
-wybor;
+menu_wybor;
 done
 }
 menu;
